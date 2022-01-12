86f406daadae574242c80819008218268d344d86
Remy Dupont <remy.dupont2@gmail.com>
2022-01-12 22:49:47 +0900

    Add light theme

--- app/src/main/java/com/remydupont/notescleanarchitecture/ui/theme/Theme.kt
+++ app/src/main/java/com/remydupont/notescleanarchitecture/ui/theme/Theme.kt
@@ -7,12 +7,31 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+private val LightColorPalette = lightColors(
+    primary = DarkGray,
+    onPrimary = Color.White,
+    background = Color.White,
+    onBackground = DarkGray,
+    surface = Color.White,
+    onSurface = DarkGray,
+
+    /*
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6),
+    secondaryVariant = Color(0xFF018786),
+    error = Color(0xFFB00020),
+    onSecondary = Color.Black,
+    onError = Color.White
+    */
+)
+
 private val DarkColorPalette = darkColors(
     primary = Color.White,
+    onPrimary = DarkGray,
     background = DarkGray,
     onBackground = Color.White,
     surface = LightBlue,
-    onSurface = DarkGray
+    onSurface = Color.White
 )
 
 @Composable
@@ -21,8 +40,10 @@ fun NotesCleanArchitectureTheme(
     content: @Composable() () -> Unit
 ) {
 
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+
     MaterialTheme(
-        colors = DarkColorPalette,
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
